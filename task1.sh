045a575d41757bce327bc0cb46c056c9067f755b
skill-alba <167703984+skill-alba@users.noreply.github.com>
2024-05-31 22:27:56 +0300

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -21,4 +21,7 @@ echo Apache is installed
 echo Python is installed
 echo SSH-server is installed and started
 mkdir /home/skill/SkillFactory_HW
+ufw allow ssh comment 'Open port ssh tcp port22'
+ufw allow 80 comment 'Open 80 port HTTP'
+ufw allow 443 comment 'Open 443 port for HTTPS'
 date "+%d-%m-%Y %H-%M-%S"
